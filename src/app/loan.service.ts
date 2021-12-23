855be73d83b6838952570b65f48ab35bd7a49ce9
NatWilleme <nathanwilleme@gmail.com>
2021-12-23 15:09:44 +0100

    Loan service, suppression du header token

--- src/app/loan.service.ts
+++ src/app/loan.service.ts
@@ -2,26 +2,23 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Loan } from './loan';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoanService {
 
-  private headers: HttpHeaders;
-  private accessPointUrl: string =  'http://localhost:57191/api/Loans';
+  private accessPointUrl: string = environment.urlPicsou + '/Loans';
 
   constructor(private http: HttpClient) {
-    this.headers = new HttpHeaders({
-      'x-auth-token': 'D4x5PY9PcEGzEmtkA1YP4A=='
-    });
    }
-
-  getLoansByUser(userID:number): Observable<any> {
-    return this.http.get<Loan>(this.accessPointUrl + "/" + userID, { headers : this.headers });
-  }
-
+   
+   getLoansByUser(userID:number): Observable<any> {
+     return this.http.get<Loan>(this.accessPointUrl + "/" + userID);
+    }
+    
   addLoan(loan: Loan): Observable<Loan> {
-    return this.http.post<Loan>(this.accessPointUrl + "/" + localStorage.getItem("userID"), loan, { headers: this.headers });
+    return this.http.post<Loan>(this.accessPointUrl + "/" + localStorage.getItem("userID"), loan);
   }
 }
